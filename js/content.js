9efc81e8e7022bfbe7b672d175dde52fab3066b6
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-10-15 13:34:45 +1100

    Initial commit

---
+++ js/content.js
@@ -0,0 +1,34 @@
+// const dialogWrapper = document.createElement("div");
+// body = document.querySelector("body");
+// body.insertAdjacentElement("afterend", dialogWrapper);
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const closeBtn = document.querySelector('.close');
+
+const computerBtn = document.getElementById("computer-button");
+const elearningBtn = document.getElementById("elearning-button")
+
+// Events
+computerBtn.addEventListener('click', openModal);
+elearningBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+// window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
